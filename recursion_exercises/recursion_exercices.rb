17964e138c6c81bc7b13b344994f4a784f297cc0
App Academy Student <aa@Apps-Mac-mini-72.local>
2022-08-03 12:16:30 -0400

    range

--- recursion_exercises/recursion_exercices.rb
+++ recursion_exercises/recursion_exercices.rb
@@ -1,1 +1,22 @@
-# yoooooooooooo
+def iter_range(first_ele, last_ele)
+  return [] if last_ele < first_ele
+
+  result = []
+  (first_ele...last_ele).each do |i|
+    result << i
+  end
+
+  result
+
+end
+
+def recur_range(first_ele, last_ele)
+  return [] if last_ele < first_ele
+  return [first_ele] if last_ele == first_ele + 1
+
+  recur_range(first_ele, first_ele+1) + recur_range(first_ele+1, last_ele)
+  
+end
+
+  # p iter_range(2, 7)
+  p recur_range(2,7)
